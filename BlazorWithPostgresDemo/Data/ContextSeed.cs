9e536323d645630513bb9ee0279f0a8d7cb01a0e
Dyako Baram <dyako.baram@gmail.com>
2021-08-18 18:15:12 +0300

    init commit

---
+++ BlazorWithPostgresDemo/Data/ContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+
+namespace BlazorWithPostgresDemo.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.Roles.Any())
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin")); 
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+            
+        }
+        public static async Task SeedSuperAdminAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default Us
+            var defaultUser = new IdentityUser() 
+            { 
+                UserName = "dyako", 
+                Email = "dyako.baram@gmail.com",
+                EmailConfirmed = true, 
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if(user==null)
+                {
+                    await userManager.CreateAsync(defaultUser, "Assall01!");
+                    await userManager.AddToRoleAsync(defaultUser, "Admin");
+                }
+            }
+        }
+    }
+}
---
+++ BlazorWithPostgresDemo/Data/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorWithPostgresDemo.Data
+{   [Table("Products")]
+    public class Products
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Column("product_name")]
+        [MaxLength(255)]
+        public string ProductName { get; set; }
+        [Column("date_bought")]
+        public DateTime DateBought { get; set; }
+    }
+}
